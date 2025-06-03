75e08b01396fbd79d102a17353fba8dff5269513
Holden Grissett <holdeng96@gmail.com>
2025-06-02 17:58:34 -0700

    render environment
    Signed-off-by: Holden Grissett <holdeng96@gmail.com>

--- pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py
+++ pvp/experiments/metadrive/train_pvp_metadrive_fakehuman.py
@@ -7,7 +7,7 @@ from pvp.experiments.metadrive.egpo.fakehuman_env import FakeHumanEnv
 from pvp.pvp_td3 import PVPTD3
 from pvp.sb3.common.callbacks import CallbackList, CheckpointCallback
 from pvp.sb3.common.monitor import Monitor
-from pvp.sb3.common.vec_env import SubprocVecEnv
+from pvp.sb3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from pvp.sb3.common.wandb_callback import WandbCallback
 from pvp.sb3.haco import HACOReplayBuffer
 from pvp.sb3.td3.policies import TD3Policy
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     parser.add_argument("--wandb", action="store_true", help="Set to True to upload stats to wandb.")
     parser.add_argument("--wandb_project", type=str, default="", help="The project name for wandb.")
     parser.add_argument("--wandb_team", type=str, default="", help="The team name for wandb.")
-    parser.add_argument("--log_dir", type=str, default="/home/zhenghao/pvp", help="Folder to store the logs.")
+    parser.add_argument("--log_dir", type=str, default="/home/holdengs/pvp4real", help="Folder to store the logs.")
     parser.add_argument("--free_level", type=float, default=0.95)
     parser.add_argument("--bc_loss_weight", type=float, default=0.0)
     parser.add_argument("--with_human_proxy_value_loss", default="True", type=str)
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         env_config=dict(
 
             # Original real human exp env config:
-            # use_render=True,  # Open the interface
+            use_render=True,  # Open the interface
             # manual_control=True,  # Allow receiving control signal from external device
             # controller=control_device,
             # window_size=(1600, 1100),
@@ -126,20 +126,21 @@ if __name__ == '__main__':
     assert config["algo"]["env"] is not None
 
     # ===== Also build the eval env =====
-    def _make_eval_env():
-        eval_env_config = dict(
-            use_render=False,  # Open the interface
-            manual_control=False,  # Allow receiving control signal from external device
-            start_seed=1000,
-            horizon=1500,
-        )
-        from pvp.experiments.metadrive.human_in_the_loop_env import HumanInTheLoopEnv
-        from pvp.sb3.common.monitor import Monitor
-        eval_env = HumanInTheLoopEnv(config=eval_env_config)
-        eval_env = Monitor(env=eval_env, filename=str(trial_dir))
-        return eval_env
+    # def _make_eval_env():
+    #     eval_env_config = dict(
+    #         use_render=False,  # Open the interface
+    #         manual_control=False,  # Allow receiving control signal from external device
+    #         start_seed=1000,
+    #         horizon=1500,
+    #     )
+    #     from pvp.experiments.metadrive.human_in_the_loop_env import HumanInTheLoopEnv
+    #     from pvp.sb3.common.monitor import Monitor
+    #     eval_env = HumanInTheLoopEnv(config=eval_env_config)
+    #     eval_env = Monitor(env=eval_env, filename=str(trial_dir))
+    #     return eval_env
 
-    eval_env = SubprocVecEnv([_make_eval_env])
+    # eval_env = DummyVecEnv([_make_eval_env])
+    eval_env = None
 
     # ===== Setup the callbacks =====
     save_freq = args.save_freq  # Number of steps per model checkpoint
@@ -174,12 +175,6 @@ if __name__ == '__main__':
         total_timesteps=50_000,
         callback=callbacks,
         reset_num_timesteps=True,
-
-        # eval
-        eval_env=eval_env,
-        eval_freq=150,
-        n_eval_episodes=50,
-        eval_log_path=str(trial_dir),
 
         # logging
         tb_log_name=experiment_batch_name,
